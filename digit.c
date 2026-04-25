4976d3e9549c7a8e9054c542f6dc12414d38d6a0
Pavan-kumar-130507 <pavankumar130507@gmail.com>
2026-04-25 11:56:58 +0530

    Add files via upload

---
+++ digit.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+char str[10];
+printf("enter the string:\n");
+scanf("%s",str);
+for(int i=0;i<=strlen(str)-1;i++)
+{
+    if(str[i]>='0'&&str[i]<='9')
+    {
+        printf("%c\t",str[i]);
+    }
+}
+}
