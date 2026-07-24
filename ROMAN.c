5136e2686eb2546e8053f20b23e1956dff033c14
Pavan-kumar-130507 <pavankumar130507@gmail.com>
2026-07-24 15:20:28 +0530

    Update ROMAN.c

--- ROMAN.c
+++ ROMAN.c
@@ -1,48 +1,53 @@
-#include<stdio.h>
-#include<string.h>
+ #include <stdio.h>
+#include <string.h>
+
 int value(char ch)
 {
-	if(ch=='I')
-	{
-		return 1;
-	}
-	else if(ch=='V')
-	{
-		return 5;
-	}
-	else if(ch=='X')
-	{
-		return 10;
-	}
-	else if(ch=='L')
-	{
-		return 50;
-	}
-	return 0;
+    if(ch == 'I')
+        return 1;
+    else if(ch == 'V')
+        return 5;
+    else if(ch == 'X')
+        return 10;
+    else if(ch == 'L')
+        return 50;
+    return 0;
 }
-void main()
+
+int main()
 {
-	char str[10];
-	printf("enter the string:\n");
-	scanf("%s",str);
-	int sum=0;
-	for(int i=0;i<=strlen(str)-1;i++)
-	{
-		int num1=value(str[i]);
-		int num2=value(str[i+1]);
-		if(i==strlen(str)-1))
-		{
-			sum=sum+num1;
-	    }
-		else if(num1<num2)
-		{
-			sum=sum+(num2-num1);
-			i++;
-		}
-		else if(num1>=num2)
-		{
-			sum=sum+num1;
-		}
-	}
-	printf("%d",sum);
+    char str[20];
+    printf("Enter the Roman numeral: ");
+    scanf("%19s", str);
+
+    int sum = 0;
+    int len = strlen(str);
+
+    for(int i = 0; i < len; i++)
+    {
+        int num1 = value(str[i]);
+
+        if(i == len - 1)
+        {
+            sum += num1;
+        }
+        else
+        {
+            int num2 = value(str[i + 1]);
+
+            if(num1 < num2)
+            {
+                sum += (num2 - num1);
+                i++;
+            }
+            else
+            {
+                sum += num1;
+            }
+        }
+    }
+
+    printf("Decimal value = %d\n", sum);
+
+    return 0;
 }
